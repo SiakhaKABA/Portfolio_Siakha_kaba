5344323929b8cb500fd370849b5e0489c3405cf9
Siakha KABA <siakha.kaba94@gmail.com>
2026-04-11 10:10:41 +0000

    feat(fonction charger-projet) : Définition de la fonction chargerProjet depuis db.json via api REST

--- v3/script.js
+++ v3/script.js
@@ -138,3 +138,17 @@ async function supprimerProjet(id) {
   }
 }
 
+async function chargerProjets() {
+  try {
+    const response = await fetch(API_URL);
+    projets = await response.json();
+    projets.forEach(p => {
+      creerProjet(p.id, p.libelle, p.image);
+    });
+  } catch (error) {
+    console.error("Erreur chargement projets :", error);
+  }
+}
+
+// Lancement au démarrage
+chargerProjets();
